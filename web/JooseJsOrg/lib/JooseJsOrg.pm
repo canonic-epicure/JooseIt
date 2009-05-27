a5989f3483017c0b993fc8863107de10119e33f0
SamuraiJack <nickolay8@gmail.com>
2009-05-27 17:50:14 +0400

    - initial commit

---
+++ web/JooseJsOrg/lib/JooseJsOrg.pm
@@ -0,0 +1,72 @@
+package JooseJsOrg;
+
+use strict;
+use warnings;
+
+use Catalyst::Runtime '5.70';
+
+use parent qw/Catalyst/;
+
+use Catalyst qw/
+    -Debug
+    ConfigLoader
+    Static::Simple
+/;
+
+
+our $VERSION = '0.01';
+
+__PACKAGE__->config( name => 'JooseJsOrg' );
+
+# Start the application
+__PACKAGE__->setup();
+
+
+
+# Set flags and add plugins for the application
+#
+#         -Debug: activates the debug mode for very useful log messages
+#   ConfigLoader: will load the configuration from a Config::General file in the
+#                 application's home directory
+# Static::Simple: will serve static files from the application's root 
+#                 directory
+
+# Configure the application. 
+#
+# Note that settings in joosejsorg.conf (or other external
+# configuration file that you set up manually) take precedence
+# over this when using ConfigLoader. Thus configuration
+# details given here can function as a default configuration,
+# with a external configuration file acting as an override for
+# local deployment.
+
+
+
+=head1 NAME
+
+JooseJsOrg - Catalyst based application
+
+=head1 SYNOPSIS
+
+    script/joosejsorg_server.pl
+
+=head1 DESCRIPTION
+
+[enter your description here]
+
+=head1 SEE ALSO
+
+L<JooseJsOrg::Controller::Root>, L<Catalyst>
+
+=head1 AUTHOR
+
+SamuraiJack
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ web/JooseJsOrg/lib/JooseJsOrg/View/TT.pm
@@ -0,0 +1,31 @@
+package JooseJsOrg::View::TT;
+
+use strict;
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(TEMPLATE_EXTENSION => '.tt');
+
+=head1 NAME
+
+JooseJsOrg::View::TT - TT View for JooseJsOrg
+
+=head1 DESCRIPTION
+
+TT View for JooseJsOrg. 
+
+=head1 AUTHOR
+
+=head1 SEE ALSO
+
+L<JooseJsOrg>
+
+SamuraiJack
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
