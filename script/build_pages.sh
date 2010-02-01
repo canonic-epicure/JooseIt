9d34ab84b207a120b947cf430f1c359d4d2081cf
SamuraiJack <nickolay8@gmail.com>
2010-02-01 23:12:29 +0300

    - JooseIt.Widget.Contacts renamed to JooseIt.Widget.Resources - started pages building script

---
+++ script/build_pages.sh
@@ -0,0 +1,4 @@
+MultiMarkdown.pl lib/JooseIt/Widget/Home.mmd > lib/JooseIt/Widget/Home.html
+MultiMarkdown.pl lib/JooseIt/Widget/About.mmd > lib/JooseIt/Widget/About.html
+MultiMarkdown.pl lib/JooseIt/Widget/Download.mmd > lib/JooseIt/Widget/Download.html
+MultiMarkdown.pl lib/JooseIt/Widget/Resources.mmd > lib/JooseIt/Widget/Resources.html
