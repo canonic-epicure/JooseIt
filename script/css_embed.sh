587ff5f2c5e7557ea38aced8da952b9ef0d212bd
SamuraiJack <nickolay8@gmail.com>
2010-03-05 11:53:58 +0300

    - some progress on embedding images into data uris

--- script/css_embed.sh
+++ script/css_embed.sh
@@ -1,1 +1,9 @@
-java -jar bin/cssembed-0.3.2.jar blib/lib/JooseIt/static/css/JooseIt.css > JooseIt.embed.css
+ROOT=http://catalyst-dev/JavaScript/JooseIt/blib/lib/JooseIt/static/css/buttons.css
+CSS_FILE=blib/lib/JooseIt/static/css/buttons.css
+
+
+chmod 664 $CSS_FILE
+
+java -jar bin/cssembed-0.3.2.jar --mhtml --mhtmlroot $ROOT $CSS_FILE > $CSS_FILE.mhtml
+
+mv $CSS_FILE.mhtml $CSS_FILE
